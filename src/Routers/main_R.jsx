cc25d81a0d96762edc3894b9001b80c9b346fe6d
amird55 <amir.dgn@outlook.com>
2026-03-24 21:12:01 +0200

    הוספת עמוד סטטי שני - אודות

--- src/Routers/main_R.jsx
+++ src/Routers/main_R.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import {createBrowserRouter} from "react-router";
 import EmptyLayout from "../LayOut/EmptyLayout";
 import HomePage from "../_Features/HomePage/HomePage.jsx";
+import About from "../_Features/About/About.jsx";
 
 const router = createBrowserRouter([
     {
         element:<EmptyLayout />,
         children:[
             {path:"/",         element:<HomePage />,     },
-            {path:"/home",     element:<HomePage />,     },
+            {path:"/About",     element:<About />,     },
         ]
     },
 ]);
---
+++ src/_Features/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About(props) {
+    return (
+        <div>
+            מאגר השאלות שתמיד רצית אך לא ידעת לשאול
+        </div>
+    );
+}
+
+export default About;
