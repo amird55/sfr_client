9bf0c6400db4bdc66b8b8059d53bf64a5a078afb
amird55 <amir.dgn@outlook.com>
2026-05-05 17:40:25 +0300

    add query to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ import { prefixer } from 'stylis';
 import { ThemeProvider } from '@mui/material';
 import { CssBaseline } from '@mui/material';
 import { theme } from './theme_params.jsx';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 // Create rtl cache
 const cacheRtl = createCache({
@@ -23,12 +26,14 @@ const cacheRtl = createCache({
 function App() {
 
   return (
-    <CacheProvider value={cacheRtl}>
-        <ThemeProvider theme={theme}>
-            <CssBaseline /> {/* This normalizes the styles */}
-            <RouterProvider router={router} />
-        </ThemeProvider>
-    </CacheProvider>
+    <QueryClientProvider client={queryClient}>
+        <CacheProvider value={cacheRtl}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline /> {/* This normalizes the styles */}
+                <RouterProvider router={router} />
+            </ThemeProvider>
+        </CacheProvider>
+    </QueryClientProvider>
   )
 }
 
